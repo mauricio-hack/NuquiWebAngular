556456bf487b3c54f7590264fe95cbbd678a9287
Mauricio2 <mauricio.velez.osorno.14@gmail.com>
2020-07-18 16:02:26 -0500

    se crean metodos para login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projectNuquiweb';
+
+  constructor(public afAuth: AngularFireAuth){ }
+
+  // tslint:disable-next-line: typedef
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  // tslint:disable-next-line: typedef
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
 }
